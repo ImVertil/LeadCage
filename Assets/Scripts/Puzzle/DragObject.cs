a5f24332b6b5d61ce4abad563b1694bcd51a551d
Tomasz Bajera <99763277+ImVertil@users.noreply.github.com>
2023-01-09 04:46:47 +0100

    Added cable snapping

--- Assets/Scripts/Puzzle/DragObject.cs
+++ Assets/Scripts/Puzzle/DragObject.cs
@@ -4,6 +4,7 @@ public class DragObject : MonoBehaviour
 {
     private Vector3 _offset;
     private float _screenPointZ;
+    private GameObject _snapTo;
 
     private void OnMouseDown()
     {
@@ -14,6 +15,26 @@ public class DragObject : MonoBehaviour
     private void OnMouseDrag()
     {
         transform.position = GetMouseWorldPosition() + _offset;
+    }
+
+    private void OnMouseUp()
+    {
+        if(_snapTo is not null)
+        {
+            transform.position = new Vector3(_snapTo.transform.position.x, _snapTo.transform.position.y, transform.position.z);
+        }
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        Debug.Log("enter");
+        _snapTo = collider.gameObject;
+    }
+
+    private void OnTriggerExit(Collider collider)
+    {
+        Debug.Log("exit");
+        _snapTo = null;
     }
 
     private Vector3 GetMouseWorldPosition()
