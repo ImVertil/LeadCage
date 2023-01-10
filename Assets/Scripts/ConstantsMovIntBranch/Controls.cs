e2b532c8402791be8228d25a2b5e8885fa9976e6
Veletor <upadacteztrzebaladnie@mailfence.com>
2023-01-10 01:21:08 +0100

    Dodalem proste poruszanie sie z wykorzystaniem fizyki
    +Poruszanie kamera
    +Poruszanie graczem
    +Tarcie
    +Limit predkosci
    +Dodano probuilder do tworzenia testowych modeli

---
+++ Assets/Scripts/ConstantsMovIntBranch/Controls.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Trzeba to potem zmergowac albo zastapic w zunifikowanym folderze constants
+
+public static class Controls
+{
+    public const string HORIZONTAL = "Horizontal";
+    public const string VERTICAL = "Vertical";
+    public const string JUMP = "Jump";
+    public const string MOUSEX = "Mouse X";
+    public const string MOUSEY = "Mouse Y";
+    
+}
