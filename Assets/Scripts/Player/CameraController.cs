bdc51e0bd79fd222bf92ba571b056b3b12f968cc
Veletor <upadacteztrzebaladnie@mailfence.com>
2023-01-25 13:37:03 +0100

    Recoil still doesnt work vertically yet, but it do work horizontally!!!

--- Assets/Scripts/Player/CameraController.cs
+++ Assets/Scripts/Player/CameraController.cs
@@ -13,20 +13,22 @@ public class CameraController : MonoBehaviour
     }
     
     private float _xRotation = 0f;
-    private float _yRotation = 0f;
+    private float _desiredX;
+
     void Update()
     {
         float mouseX = Input.GetAxis(Controls.MOUSEX) * _mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis(Controls.MOUSEY) * _mouseSensitivity * Time.deltaTime;
 
+        Vector3 rot = transform.localRotation.eulerAngles;
+        _desiredX = rot.y + mouseX;
+
         // Troche wydaje sie na odwrot, ale mouseX/Y maja wartosc wzdluz osi a rotacja dziala "Wokol" osi
         
         _xRotation -= mouseY;
         _xRotation = Mathf.Clamp(_xRotation, -90f, 90f);
-        
-        _yRotation += mouseX;
 
-        transform.rotation = Quaternion.Euler(_xRotation, _yRotation, 0f);
-        _playerBody.rotation = Quaternion.Euler(0, _yRotation, 0);
+        transform.rotation = Quaternion.Euler(_xRotation, _desiredX, 0f);
+        _playerBody.rotation = Quaternion.Euler(0, _desiredX, 0);
     }
 }
