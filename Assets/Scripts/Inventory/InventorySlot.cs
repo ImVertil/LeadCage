d54c28ca8fc9213652c0bb7e8c022e2797b29bb7
Tomasz Bajera <99763277+ImVertil@users.noreply.github.com>
2023-02-02 03:51:45 +0100

    Added InventorySlot class & code adjustments

---
+++ Assets/Scripts/Inventory/InventorySlot.cs
@@ -0,0 +1,9 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventorySlot : MonoBehaviour
+{
+    public Image _slotImageComponent;
+    public TextMeshProUGUI _slotTextComponent;
+}
