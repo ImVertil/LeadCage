954fe794d472b3675786ec7f6ff98e0c578270c0
Tomasz Bajera <99763277+ImVertil@users.noreply.github.com>
2023-03-17 18:17:20 +0100

    Added sound system

---
+++ Assets/Scripts/Sound/InteractableSoundScript2.cs
@@ -0,0 +1,21 @@
+
+using UnityEngine;
+
+public class InteractableSoundScript2 : MonoBehaviour, IInteractable
+{
+    public void OnStartLook()
+    {
+        Debug.Log("2 Start Look");
+    }
+
+    public void OnEndLook()
+    {
+        Debug.Log("2 End Look");
+    }
+
+    public void OnInteract()
+    {
+        SoundManager.Instance.PlaySound(Sound.Test2, new Vector3(-30,60,0));
+    }
+}
+
