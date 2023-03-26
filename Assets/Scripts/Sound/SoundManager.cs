039102940480ae6d7f54a706339ebf21f5f30c17
Tomasz Bajera <99763277+ImVertil@users.noreply.github.com>
2023-03-26 22:51:49 +0200

    Added looped sounds & stop sound features

--- Assets/Scripts/Sound/SoundManager.cs
+++ Assets/Scripts/Sound/SoundManager.cs
@@ -36,15 +36,17 @@ public class SoundManager : MonoBehaviour
         }
     }
 
-    public void PlaySound(Sound sound, Transform transform)
+    public void PlaySound(Sound sound, Transform transform, bool loop) // if loop is set to true remember to use StopSound to turn the clip off
     {
         GameObject soundObject = _pool.Get();
         AudioSource audioSource = soundObject.GetComponent<AudioSource>();
         soundObject.transform.position = Vector3.zero;
         soundObject.transform.SetParent(transform, false);
         audioSource.clip = GetAudioClip(sound);
+        audioSource.loop = loop;
         audioSource.Play();
-        StartCoroutine(WaitAndRelease(soundObject, audioSource.clip.length));
+        if (!loop)
+            StartCoroutine(WaitAndRelease(soundObject, audioSource.clip.length));
     }
 
     public void PlaySound(Sound sound)
@@ -54,9 +56,28 @@ public class SoundManager : MonoBehaviour
         audioSource.PlayOneShot(clip);
     }
 
+    public void StopSound(Sound sound, Transform transform) 
+    {
+        AudioSource[] audioSources = transform.GetComponentsInChildren<AudioSource>();
+        foreach(AudioSource audioSource in audioSources)
+        {
+            Sound clipSound = GetSound(audioSource.clip);
+            if (audioSource.isPlaying && clipSound == sound)
+            {
+                audioSource.Stop();
+                StartCoroutine(WaitAndRelease(audioSource.gameObject, 0));
+            }
+        }
+    }
+
     public AudioClip GetAudioClip(Sound sound)
     {
         return Array.Find(sounds, s => s.name == sound).audioClip;
+    }
+
+    public Sound GetSound(AudioClip clip)
+    {
+        return Array.Find(sounds, s => s.audioClip == clip).name;
     }
 
     private IEnumerator WaitAndRelease(GameObject obj, float time)
@@ -82,10 +103,10 @@ public class SoundManager : MonoBehaviour
 
     private void OnReturnSoundObject(GameObject obj)
     {
-        if (obj.transform.parent != null)
-        {
-            obj.transform.SetParent(null);
-        }
+        AudioSource audioSource = obj.GetComponent<AudioSource>();
+        obj.transform.SetParent(null);
+        audioSource.loop = false;
+
         obj.SetActive(false);  
     }
 
