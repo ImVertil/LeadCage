13803e2d00d3eadfc93ae0c2cf8abf033f5f2448
Tomasz Bajera <99763277+ImVertil@users.noreply.github.com>
2023-03-26 18:38:25 +0200

    Opening inventory now freezes camera/player

--- Assets/Scripts/Inventory/Inventory.cs
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -50,11 +50,15 @@ public class Inventory : MonoBehaviour
         {
             Cursor.visible = false;
             Cursor.lockState = CursorLockMode.Locked;
+            PlayerController.CanMove = true;
+            PlayerController.CanMoveCamera = true;
         }
         else
         {
             Cursor.visible = true;
             Cursor.lockState = CursorLockMode.Confined;
+            PlayerController.CanMove = false;
+            PlayerController.CanMoveCamera = false;
         }
         _isShown = !_isShown;
     }
