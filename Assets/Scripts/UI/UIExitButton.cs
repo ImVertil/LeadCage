becfce39f33f9ccd4665cee416acd421c753e95d
Tomasz Bajera <99763277+ImVertil@users.noreply.github.com>
2023-04-22 23:04:01 +0200

    Added menus

---
+++ Assets/Scripts/UI/UIExitButton.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIExitButton : MonoBehaviour
+{
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("QUIT");
+    }
+}
