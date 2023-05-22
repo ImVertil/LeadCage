e2af727226f23641e8992aa18d37fce9660dad38
Tomasz Bajera <99763277+ImVertil@users.noreply.github.com>
2023-05-22 04:20:52 +0200

    Saved scripts

--- Assets/Scripts/Sound/Sound.cs
+++ Assets/Scripts/Sound/Sound.cs
@@ -1,4 +1,4 @@
 public enum Sound
 {
-    Countdown, Puzzle1_Buzzer, Footsteps, Door_Open, Door_Close, Shoot, Holster, MenuButtonHover, MenuButtonPress, BGMThing
+    Countdown, Puzzle1_Buzzer, Footsteps, Door_Open, Door_Close, Shoot, Holster, MenuButtonHover, MenuButtonPress, BGMThing,
 }
