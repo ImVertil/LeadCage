4b6b913114256607c63661b1ab7011be1602a933
Tomasz Bajera <99763277+ImVertil@users.noreply.github.com>
2023-05-28 02:26:49 +0200

    Sound system additions
    - Slight code rewrite providing us more options
    - Added events for audio sources that are outside of the sound object loop
    - Updated the VolumeHandler class to properly handle volume changes

--- Assets/Scripts/LightsTrigger.cs
+++ Assets/Scripts/LightsTrigger.cs
@@ -6,8 +6,8 @@ public class LightsTrigger : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        LightManager.Instance.TurnOffLightsGlobal();
-        LightManager.Instance.TurnOffAlarm();
+        //LightManager.Instance.TurnOffLightsGlobal();
+        //LightManager.Instance.TurnOffAlarm();
         SoundManager.Instance.PlaySound(Sound.Footsteps, this.transform, false);
         //SoundManager.Instance.PlaySound(Sound.BGMThing);
     }
--- Assets/Scripts/Sound/SoundManager.cs
+++ Assets/Scripts/Sound/SoundManager.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Pool;
 
@@ -18,14 +19,18 @@ public class SoundManager : MonoBehaviour
         {
             Destroy(this);
         }
+        SoundEvents.OnVolumeChanged += UpdateSoundVolume;
     }
     #endregion
     [SerializeField] private int _simultaneousSoundsAmount;
     private ObjectPool<GameObject> _pool;
+    private List<GameObject> _activeFromPool = new List<GameObject>();
     private GameObject _oneShotSoundObject;
+    private GameObject _bgmSoundObject;
     public int volume = 50;
     public int sfxVolume = 100;
     public int bgmVolume = 100;
+    public int ambientVolume = 100;
     public SoundClip[] sounds;
 
     private void Initialize()
@@ -34,7 +39,10 @@ public class SoundManager : MonoBehaviour
         if(_oneShotSoundObject == null)
         {
             _oneShotSoundObject = new GameObject("Sound (One Shot)");
-            _oneShotSoundObject.AddComponent<AudioSource>();
+            _bgmSoundObject = new GameObject("Sound (BGM)");
+            // delete later if not needed
+            AudioSource as1 = _oneShotSoundObject.AddComponent<AudioSource>();
+            AudioSource as2 = _bgmSoundObject.AddComponent<AudioSource>();
             //_oneShotSoundObject.AddComponent<VolumeHandler>();
         }
     }
@@ -45,12 +53,13 @@ public class SoundManager : MonoBehaviour
     public void PlaySound(Sound sound, Transform transform, bool loop) // if loop is set to true remember to use StopSound to turn the clip off
     {
         GameObject soundObject = _pool.Get();
+        _activeFromPool.Add(soundObject);
         AudioSource audioSource = soundObject.GetComponent<AudioSource>();
         soundObject.transform.position = Vector3.zero;
         soundObject.transform.SetParent(transform, false);
         audioSource.clip = GetAudioClip(sound);
         audioSource.loop = loop;
-        audioSource.volume = (volume / 100.0f) * (GetSoundType(sound) == SoundType.SFX ? (sfxVolume / 100.0f) : (bgmVolume / 100.0f));
+        audioSource.volume = (volume / 100f) * (GetSoundType(sound) == SoundType.SFX ? (sfxVolume / 100f) : (ambientVolume / 100f));
         audioSource.Play();
         if (!loop)
             StartCoroutine(WaitAndRelease(soundObject, audioSource.clip.length));
@@ -60,8 +69,19 @@ public class SoundManager : MonoBehaviour
     {
         AudioSource audioSource = _oneShotSoundObject.GetComponent<AudioSource>();
         AudioClip clip = GetAudioClip(sound);
-        audioSource.volume = (volume / 100.0f) * (GetSoundType(sound) == SoundType.SFX ? (sfxVolume / 100.0f) : (bgmVolume / 100.0f));
+        //audioSource.volume = (volume / 100f) * (GetSoundType(sound) == SoundType.SFX ? (sfxVolume / 100f) : (ambientVolume / 100f));
+        audioSource.volume = (volume / 100f) * (sfxVolume / 100f);
         audioSource.PlayOneShot(clip);
+    }
+
+    public void PlayMusic(Sound sound)
+    {
+        AudioSource audioSource = _bgmSoundObject.GetComponent<AudioSource>();
+        AudioClip clip = GetAudioClip(sound);
+        audioSource.volume = (volume / 100f) * (bgmVolume / 100f);
+        audioSource.clip = clip;
+        audioSource.loop = true;
+        audioSource.Play();
     }
 
     public void StopSound(Sound sound, Transform transform) 
@@ -75,6 +95,22 @@ public class SoundManager : MonoBehaviour
                 audioSource.Stop();
                 StartCoroutine(WaitAndRelease(audioSource.gameObject, 0));
             }
+        }
+    }
+
+    public void UpdateSoundVolume()
+    {
+        // Update both objects for music and 2d sfx
+        _bgmSoundObject.GetComponent<AudioSource>().volume = (volume / 100f) * (bgmVolume / 100f);
+        _oneShotSoundObject.GetComponent<AudioSource>().volume = (volume / 100f) * (sfxVolume / 100f);
+
+        // Update all active objects from pools to change their sound volume while they're playing
+        // not tested btw xD
+        foreach (var obj in _activeFromPool)
+        {
+            AudioSource audioSource = obj.GetComponent<AudioSource>();
+            SoundType type = GetSoundType(audioSource.clip);
+            audioSource.volume = (volume / 100f) * (type == SoundType.SFX ? (sfxVolume / 100f) : (ambientVolume / 100f));
         }
     }
 
@@ -101,6 +137,7 @@ public class SoundManager : MonoBehaviour
     private IEnumerator WaitAndRelease(GameObject obj, float time)
     {
         yield return new WaitForSeconds(time);
+        _activeFromPool.Remove(obj);
         _pool.Release(obj);
     }
 
@@ -112,7 +149,7 @@ public class SoundManager : MonoBehaviour
         obj.AddComponent<VolumeHandler>();
         audioSource.spatialBlend = 1;
         audioSource.dopplerLevel = 0.1f;
-        audioSource.volume = volume / 100.0f;
+        audioSource.volume = volume / 100f;
         return obj;
     }
 
