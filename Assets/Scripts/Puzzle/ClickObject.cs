e9a1163c66bc561af32d8ab6badacf2b8dcabaa2
Tomasz Bajera <99763277+ImVertil@users.noreply.github.com>
2023-10-16 04:35:39 +0200

    UI Texts improvements

--- Assets/Scripts/Puzzle/ClickObject.cs
+++ Assets/Scripts/Puzzle/ClickObject.cs
@@ -4,21 +4,31 @@ using UnityEngine;
 
 public class ClickObject : MonoBehaviour
 {
-    private Transform _defaultPos;
-    private Transform _viewManualPos;
     private bool _isDefault;
+    private Animator _animator;
+    private int _cameraDefault = Animator.StringToHash("CameraDefault");
+    private int _cameraInstruction = Animator.StringToHash("CameraInstruction");
 
     void Start()
     {
-        _defaultPos = Camera.main.transform;
         _isDefault = true;
+        _animator = GetComponentInParent<Animator>();
     }
 
     private void OnMouseDown()
     {
+        Debug.Log("MOUSE DOWN");
         if(_isDefault)
         {
-            
+            Debug.Log("TRUE -> INSTR");
+            _animator.SetTrigger(_cameraInstruction);
+            _isDefault = false;
+        }
+        else
+        {
+            Debug.Log("FALSE -> DEFAULT");
+            _animator.SetTrigger(_cameraDefault);
+            _isDefault = true;
         }
     }
 }
