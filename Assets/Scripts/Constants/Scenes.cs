ec5b49c6279d209a454d840adde642282a190256
Tomasz Bajera <99763277+ImVertil@users.noreply.github.com>
2024-01-22 15:26:38 +0100

    Some model import changes

--- Assets/Scripts/Constants/Scenes.cs
+++ Assets/Scripts/Constants/Scenes.cs
@@ -1,6 +1,7 @@
 public static class Scenes
 {
-    public const string GAME = "Gameplay";
+    //public const string GAME = "Gameplay";
+    public const string GAME = "!TB_Gameplay";
     public const string LOADING_SCREEN = "Loading Screen";
     public const string MAIN_MENU = "Main Menu";
     public const string PAUSE = "Pause";
